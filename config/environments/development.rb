a576eb9a7f2c77265645cb3f37e789be32868968
dev <dev@iwasaki-no-iMac.local>
2016-02-14 23:26:56 +0900

    no message

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,14 +43,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
-      :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'heroku.com',
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => 'example.com',
+      :user_name            => ENV['SMTP_USERNAME'],
+      :password             => ENV['SMTP_PASSWORD'],
+      :authentication       => 'plain',
       :enable_starttls_auto => true
   }
+
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
